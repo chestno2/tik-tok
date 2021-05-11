5da824dc9efcdbc1a0f1fb4d3a140af7c7b9f22a
Your Name <atuly152001@gmail.com>
2021-05-11 21:40:51 +0530

    initial commit

---
+++ src/fb.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDZ8QHy8qWRK2dovEO36eCeRiOXbBCpgGI",
+    authDomain: "tiktok-eff76.firebaseapp.com",
+    projectId: "tiktok-eff76",
+    storageBucket: "tiktok-eff76.appspot.com",
+    messagingSenderId: "363920608944",
+    appId: "1:363920608944:web:bc704d2e212828b366f575",
+    measurementId: "G-97FM5LLPV2"
+  };
