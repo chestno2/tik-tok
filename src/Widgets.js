657e098e750a4444cf2bb6e367387b0d6dfa36ad
Your Name <atuly152001@gmail.com>
2021-05-14 22:45:36 +0530

    widgets added

--- src/Widgets.js
+++ src/Widgets.js
@@ -3,7 +3,11 @@ import React from 'react'
 function Widgets() {
     return (
         <div style={{flex:"0.33"}}>
-            I am a Widget
+         
+         <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Ffb.watch%2Fv%2F1W29USzmJ%2F&width=500&show_text=true&height=412&appId"
+          width="500" height="412" style={{border:"none",overflow:"hidden"}} 
+          scrolling="no" frameborder="0" allowfullscreen="true" 
+         allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </div>
     )
 }
